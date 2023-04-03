9138274ba4f69767873a73ca9512d0b398e2aefe
Christofer Patrick Paes <49449786+christoferpaes@users.noreply.github.com>
2023-04-03 11:24:41 -0700

    Update SavedGame.swift

--- Color Pop/Color Pop/SavedGame.swift
+++ Color Pop/Color Pop/SavedGame.swift
@@ -2,8 +2,8 @@
 //  SavedGame.swift
 //  Color Pop
 //
-//  Created by Valentina Carfagno on 5/9/19.
-//  Copyright © 2019 RSC. All rights reserved.
+//  Created by Christofer Patrick Paes on 5/9/19.
+//  Copyright © 2019 Christofer Patrick Paes RSC. All rights reserved.
 //
 
 import Foundation
